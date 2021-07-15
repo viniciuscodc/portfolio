24246f34ba82a7229dd4dd9d0146db373feabd95
viniciuscodc <viniciuscodc@gmail.com>
2021-07-14 23:35:38 -0300

    added null function and code optimization in projectPage

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import * as React from "react";
 import "./../style/reset.css";
 import "./../style/projects.scss";
 import Navigation from "./../templates/navigation";
@@ -8,87 +8,72 @@ import image from "./../images/image-1.png";
 
 const projects = [
   {
-    description: "Website feito para a concessionária Polaris em Curitiba. O website é responsivo e foi desenhado no Adobe XD , feito usando HTML, CSS, Javascript e PHP para a página de contato.",
+    description:
+      "Website feito para a concessionária Polaris em Curitiba. O website é responsivo e foi desenhado no Adobe XD , feito usando HTML, CSS, Javascript e PHP para a página de contato.",
     tag_1: "CSS",
     tag_2: "HTML",
     tag_3: "Javascript",
   },
   {
-    description: "Descrição 2",
-    tag_1: "CSS",
+    description:
+      "Website feito para a concessionária Polaris em Curitiba. O website é responsivo e foi desenhado no Adobe XD , feito usando HTML, CSS, Javascript e PHP para a página de contato.",
+    tag_1: "CSS2",
+    tag_2: "HTML2",
+    tag_3: "Javascript",
   },
 ];
 
-/*
-for (let i = 0; i < projects.length; i++) {
-  if(typeof projects[0].tag_2 !== "undefined"){
-    //let tagContainer = document.querySelectorAll(".tag-container");
-    let tagContainer = document.getElementById("x")
-    let spanTag = document.createElement("span");
-    //tagContainer.appendChild(spanTag);
+function CardTag(cardTagData) {
+  function isNotNull(x) {
+    return typeof x !== "undefined";
   }
-}
-*/
 
-//let mainContainer = React.createElement("p", { id: "x" }, "texto texto");
-
-function CardTag(cardTagData){
-
-  console.log(cardTagData.cardTagData)
-  
-  for (let i = 0; i < projects.length; i++) {
-
-    if(typeof projects[i].tag_1 !== "undefined"){
-      return(<span>{cardTagData.cardTagData.tag_1}</span>)
-    }
-    
-    else{
-      return(null);
-    }
+  let tag_1 = projects[cardTagData.cardIndex].tag_1;
+  if (isNotNull(tag_1)) {
+    let spanText = cardTagData.cardTagData.tag_1;
+    var spanElement = React.createElement("span", { key: spanText }, spanText);
   }
+
+  let tag_2 = projects[cardTagData.cardIndex].tag_2;
+  if (isNotNull(tag_2)) {
+    let spanText = cardTagData.cardTagData.tag_2;
+    var spanElement2 = React.createElement("span", { key: spanText }, spanText);
+  }
+
+  let spanList = [spanElement, spanElement2];
+
+  return spanList;
 }
 
-const ProjectPage = () => {
-
+export default function ProjectPage() {
   return (
     <main>
-  
       <Navigation />
       <Helmet title="Projects" bodyAttributes={{ class: "projects-page" }} />
       <Social />
 
       <h2 className="projects-header">Projects</h2>
-      
+
       <div className="card-grid">
-      {projects.map(card => (
-        <div className="card">
-          <div className="card-image">
-            <img src={image} alt="project-img" />
-          </div>
-          <div className="card-information">
-            <p>
-              {card.description}
-            </p>
-          </div>
-          <div className="tag-container">
-          
-            <CardTag cardTagData={card}  />
-            {
-          /*
-            <span>{card.tag_1}</span>
-            <span>{card.tag_2}</span>
-            <span>Javascript</span>
-          */}
-          </div>
-          <div className="card-button">
-            <span>Check</span>
-          </div>
-        </div>
-      ))}
+        {projects.map(function (card, index) {
+          return (
+            <div key={index} className="card">
+              <div className="card-image">
+                <img src={image} alt="card-img" />
+              </div>
+              <div className="card-information">
+                <p>{card.description}</p>
+              </div>
+              <div className="tag-container">
+                <CardTag cardTagData={card} cardIndex={index} />
+              </div>
+              <div className="card-button">
+                <span>Check</span>
+              </div>
+            </div>
+          );
+        })}
       </div>
     </main>
   );
-  
-};
-
-export default ProjectPage;
+}
